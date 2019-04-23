bed213d80b50e80c4873351ab1046895aa387f58
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-04-23 19:02:33 +0300

    Меняем имя класса и методов.

--- src/Helper/Debug/Routing.php
+++ src/Helper/Debug/Routing.php
@@ -11,15 +11,15 @@ namespace ItForFree\WpAddons\Helper\Debug;
 /**
  * Для отладки маршрутов и всего что с ними связано
  */
-class Template {
+class Routing {
     
     /**
-     * Вернет использование 
+     * Вернет текущие правила построения маршрутов
      * 
-     * @global WP_Rewrite $wp_rewrite
+     * @global WP_Rewrite $wp_rewrite  правила ядра
      * @retrun array
      */
-    public static function getCurrentFile()
+    public static function getRewriteRules()
     {
         global $wp_rewrite;  
         return $wp_rewrite->rules;
